1cc4e9d40d22729b776fbeef8814f22c12152e4a
설화 <62371681+citrusfruits6276@users.noreply.github.com>
2025-08-05 20:39:37 +0900

    [Bronze II] Title: 분산처리, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,9 @@
+test = int(input())
+
+for _ in range(test):
+    a, b = map(int, input().split())
+    result = pow(a, b, 10)
+    if result == 0:
+        print(10)
+    else:
+        print(result)
